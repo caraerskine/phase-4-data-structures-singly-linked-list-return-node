14fb236c80750a2546640fe30b61c97cbb4f5678
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-07-24 10:33:32 -0400

    commit tree f142fd3643349f73f098d101cc188f082670e3e6

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,8 +7,43 @@ class LinkedList
     @head = nil
   end
 
+ 
+
   def nth_from_end(n)
-    # your code here
+    return nil if @head.nil? || n <= 0
+
+    fast_pointer = @head
+    slow_pointer = @head
+    count = 0
+
+    # Move the fast pointer 'n' steps ahead
+    while count < n
+      return nil if fast_pointer.nil?  # The list has fewer than 'n' nodes
+      fast_pointer = fast_pointer.next_node
+      count += 1
+    end
+
+    while fast_pointer&.next_node
+      fast_pointer = fast_pointer.next_node
+      slow_pointer = slow_pointer.next_node
+    end
+
+    # The slow pointer is now pointing to the nth node from the end
+    slow_pointer&.value
   end
+  
+  def append(value)
+    if @head.nil?
+      @head = Node.new(value)
+    else
+      current = @head
+      while current.next_node
+        current = current.next_node
+      end
+      current.next_node = Node.new(value)
+    end
+  end
+
+
 
 end
